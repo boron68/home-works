48aa37a9ac637c942e14dc3b8ffd7ebd84812792
boron68 <korolyuk.stasislav@gmail.com>
2026-07-16 18:09:57 +0300

    hw7

---
+++ lessons/src/hw7/Hw7.java
@@ -0,0 +1,13 @@
+package hw7;
+
+public class Hw7 {
+    public static void main(String[] args) {
+        int totalSum = 0;
+        for(int num =1;num <= 6; num++){
+            totalSum += num;
+            System.out.println(num + ") Num is " + num + ", sum is " + totalSum);
+        }
+System.out.println("------------------------");
+        System.out.println("Sum of numbers is "+ totalSum);
+    }
+}
