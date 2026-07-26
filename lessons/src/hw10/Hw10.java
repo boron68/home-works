65bbc770deb96fb91a287c6f35a67ca5ab1f7d3f
boron68 <korolyuk.stasislav@gmail.com>
2026-07-26 18:03:18 +0300

    hw10

--- lessons/src/hw10/Hw10.java
+++ lessons/src/hw10/Hw10.java
@@ -9,14 +9,14 @@ public class Hw10 {
         int[][] matrix = new int[rows][cols];
         Random random = new Random();
 
-        // 1 & 2. Заповнення матриці випадковими числами від 1 до 50
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 matrix[i][j] = random.nextInt(50) + 1;
             }
         }
 
-        // 3. Вивід матриці на екран
+
         System.out.println("Матриця 4x4:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -26,7 +26,7 @@ public class Hw10 {
         }
         System.out.println();
 
-        // 4. Сума елементів у парних та непарних рядках
+
         int evenRowsSum = 0; // рядки 0, 2
         int oddRowsSum = 0;  // рядки 1, 3
 
@@ -40,8 +40,7 @@ public class Hw10 {
             }
         }
 
-        // 5. Добуток елементів у парних та непарних стовпцях
-        // Використовуємо long, щоб уникнути переповнення int
+
         long evenColsProduct = 1; // стовпці 0, 2
         long oddColsProduct = 1;  // стовпці 1, 3
 
@@ -55,14 +54,12 @@ public class Hw10 {
             }
         }
 
-        // 6. Вивід результатів обчислень
         System.out.println("Сума елементів у парних рядках (рядок 0, 2): " + evenRowsSum);
         System.out.println("Сума елементів у непарних рядках (рядок 1, 3): " + oddRowsSum);
         System.out.println("Добуток елементів у парних стовпцях (стовпець 0, 2): " + evenColsProduct);
         System.out.println("Добуток елементів у непарних стовпцях (стовпець 1, 3): " + oddColsProduct);
         System.out.println();
 
-        // 7. Перевірка на магічний квадрат
         boolean isMagicSquare = checkMagicSquare(matrix, rows, cols);
         if (isMagicSquare) {
             System.out.println("Матриця є магічним квадратом.");
@@ -77,7 +74,7 @@ public class Hw10 {
             targetSum += matrix[0][j];
         }
 
-        // Перевірка рядків
+
         for (int i = 1; i < rows; i++) {
             int rowSum = 0;
             for (int j = 0; j < cols; j++) {
@@ -86,7 +83,7 @@ public class Hw10 {
             if (rowSum != targetSum) return false;
         }
 
-        // Перевірка стовпців
+
         for (int j = 0; j < cols; j++) {
             int colSum = 0;
             for (int i = 0; i < rows; i++) {
@@ -95,14 +92,14 @@ public class Hw10 {
             if (colSum != targetSum) return false;
         }
 
-        // Перевірка головної діагоналі
+
         int mainDiagonalSum = 0;
         for (int i = 0; i < rows; i++) {
             mainDiagonalSum += matrix[i][i];
         }
         if (mainDiagonalSum != targetSum) return false;
 
-        // Перевірка побічної діагоналі
+
         int sideDiagonalSum = 0;
         for (int i = 0; i < rows; i++) {
             sideDiagonalSum += matrix[i][cols - 1 - i];
